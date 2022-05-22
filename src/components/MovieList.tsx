eae134975054b2e2932a4912851e71f5009f411c
Bojenko <85933647+Bojenko@users.noreply.github.com>
2022-05-22 18:32:33 +0300

    app has been created

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+interface MovieListProps{
+    ml:string[], //movie list
+    mp:string[] //movie pictures
+}
+const MovieList:FC<MovieListProps> = (ml,mp) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MovieList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+type action = {
+    type:string,
+    payload:string
+}
+const reducer = (state:any, action:action)=>{
+    return state;
+}
+const store = createStore(reducer);
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 );
 
